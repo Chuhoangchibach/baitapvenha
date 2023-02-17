fb494f2f7081a10e989c638fec600cc9b59b9f7a
Chuhoangchibach <Emthichchoidong@gmail.com>
2023-02-17 22:05:33 +0700

    comment

---
+++ chuhoangchibach_app.java
@@ -0,0 +1,11 @@
+package chuhoangchibach_project;
+
+public class chuhoangchibach_app {
+
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Chu Hoang Chi Bach hoc Java");	
+	}
+
+}
